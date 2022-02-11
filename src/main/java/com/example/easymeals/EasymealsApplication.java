630980054959587a24ae0859d3d164d334c421b1
Dima <omeluan.dima@gmail.com>
2022-02-11 20:28:34 +0200

    Init commit

---
+++ src/main/java/com/example/easymeals/EasymealsApplication.java
@@ -0,0 +1,13 @@
+package com.example.easymeals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasymealsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasymealsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/easymeals/EasymealsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.easymeals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasymealsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
