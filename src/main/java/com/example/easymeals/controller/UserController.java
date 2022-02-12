d96ff8aadc89f4fcb8567b72e3811db698b27ebf
Dima <omeluan.dima@gmail.com>
2022-02-12 21:59:14 +0200

    Fix ability to get user by id + add delete mapping

--- src/main/java/com/example/easymeals/controller/UserController.java
+++ src/main/java/com/example/easymeals/controller/UserController.java
@@ -3,23 +3,40 @@ package com.example.easymeals.controller;
 import com.example.easymeals.entity.User;
 import com.example.easymeals.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
-@Controller
-@RequiredArgsConstructor
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
 
     private final UserRepository userRepository;
 
-    @GetMapping("/users")
+    @GetMapping
     public List<User> getAll() {
         return userRepository.findAll();
+    }
+
+    // TODO regular pattern for id
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        if(!userRepository.existsById(id)) {
+            // TODO add exceptions types
+        }
+        return ResponseEntity.ok(userRepository.getById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> delete(@PathVariable Long id) {
+        if(!userRepository.existsById(id)) {
+            // TODO throw exception
+        }
+        userRepository.deleteById(id);
+        return ResponseEntity.noContent().build(); // <- when we want to return response without content
     }
 
 }
--- src/main/java/com/example/easymeals/repository/UserRepository.java
+++ src/main/java/com/example/easymeals/repository/UserRepository.java
@@ -2,8 +2,12 @@ package com.example.easymeals.repository;
 
 import com.example.easymeals.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findById(Long id);
 }
